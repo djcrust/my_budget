c80437c7f3620af9cfa108a4f444198c48611837
Alex William <djcrust@gmail.com>
2020-05-11 14:59:15 +0300

    Initial commit

---
+++ budget_settings/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BudgetSettingsConfig(AppConfig):
+    name = 'budget_settings'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\python_project\my_budget\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
